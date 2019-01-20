c9b5c18ac47ae21559b50f17ed2728325430213e
Stu Weir <cameron.weir@mytwu.ca>
2019-01-20 07:56:49 -0800

    finished leap exercise in JS

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,6 @@
 function isLeap(year) {
-  if (year % 400 === 0 || (year !== 100 && year % 4 === 0)) return true;
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) return true;
   return false;
 }
 
-export default isLeap;
+export { isLeap };
